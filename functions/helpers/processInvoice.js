1e8fb231a6d460fad70f50b72f3b2fedfa9bfced
Eugene Avila <eugene@ctoglobal.co>
2020-04-22 02:54:14 +0800

    master - added .firebase to gitignore - added package.json for both client and api side - removed auth folder - refactor processInvoice

--- functions/helpers/processInvoice.js
+++ functions/helpers/processInvoice.js
@@ -7,50 +7,51 @@ function processInvoice (auth) {
       range: 'Order Sheet!A2:V',
       spreadsheetId: '14DhGQP41H3AzfY7GhD5jxZytP8xkbnGygzap_KhVuII'
     }
+    google.sheets('v4').spreadsheets.values.get(request, (err, res) => {
+      if (err) reject(err.message)
 
-    const data = {}
-    google.sheets('v4').spreadsheets.values.get(request)
-      .then(res => {
-        const rows = res.data.values
-        if (!rows.length) resolve({})
+      const rows = res.data.values
+      const data = {}
 
-        rows.forEach((row, index) => {
-          const orderId = row[14]
-          const delivered = row[16] ? row[16] === 'YES' : false
-          if (orderId && orderId !== 'NAH' && row[3] && !delivered) {
-            data[orderId] = data[orderId] || { orders: [] }
+      if (!rows.length) resolve({})
 
-            const dataTofill = data[orderId]
+      rows.forEach((row, index) => {
+        const orderId = row[14]
+        const delivered = row[16] ? row[16] === 'YES' : false
+        if (orderId && orderId !== 'NAH' && row[3] && !delivered) {
+          data[orderId] = data[orderId] || { orders: [] }
 
-            dataTofill.dateOrdered = dataTofill.dateOrdered || row[0]
-            dataTofill.facebookName = dataTofill.facebookName || row[1]
-            dataTofill.contactPerson = dataTofill.contactPerson || row[2]
-            dataTofill.contactNumber = dataTofill.contactNumber || row[3]
-            dataTofill.address = dataTofill.address || row[4]
-            dataTofill.paymentMethod = dataTofill.paymentMethod || row[5]
-            dataTofill.paymentSent = dataTofill.paymentSent || row[10]
-            dataTofill.courier = dataTofill.courier || row[11]
-            dataTofill.trackingNumber = dataTofill.trackingNumber || row[12]
-            dataTofill.status = dataTofill.status || row[13]
-            dataTofill.orderId = dataTofill.orderId || row[14]
-            dataTofill.trackURL = dataTofill.trackURL || row[15]
-            dataTofill.shippingFee = dataTofill.shippingFee || row[17]
+          const dataTofill = data[orderId]
 
-            // Fill up orders
-            dataTofill.orders = [
-              ...dataTofill.orders,
-              {
-                quantity: row[6],
-                item: row[7],
-                unitCost: row[8],
-                amount: row[9]
-              }
-            ]
-          }
-        })
+          dataTofill.dateOrdered = dataTofill.dateOrdered || row[0]
+          dataTofill.facebookName = dataTofill.facebookName || row[1]
+          dataTofill.contactPerson = dataTofill.contactPerson || row[2]
+          dataTofill.contactNumber = dataTofill.contactNumber || row[3]
+          dataTofill.address = dataTofill.address || row[4]
+          dataTofill.paymentMethod = dataTofill.paymentMethod || row[5]
+          dataTofill.paymentSent = dataTofill.paymentSent || row[10]
+          dataTofill.courier = dataTofill.courier || row[11]
+          dataTofill.trackingNumber = dataTofill.trackingNumber || row[12]
+          dataTofill.status = dataTofill.status || row[13]
+          dataTofill.orderId = dataTofill.orderId || row[14]
+          dataTofill.trackURL = dataTofill.trackURL || row[15]
+          dataTofill.shippingFee = dataTofill.shippingFee || row[17]
+
+          // Fill up orders
+          dataTofill.orders = [
+            ...dataTofill.orders,
+            {
+              quantity: row[6],
+              item: row[7],
+              unitCost: row[8],
+              amount: row[9]
+            }
+          ]
+        }
       })
-      .catch(console.log)
-      .finally(() => resolve(data))
+
+      resolve(data)
+    })
   })
 }
 
--- functions/routes/apis.js
+++ functions/routes/apis.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const getGoogleAuth = require('../auth/getGoogleAuth')
 const processInvoice = require('../helpers/processInvoice')
 
 router.get('/invoice', async (req, res, next) => {
-  // const auth = await getGoogleAuth()
   const data = await processInvoice()
   res.json(data)
 })
